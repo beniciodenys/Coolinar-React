647079c4838b0a536a7ac01b4b86419123c0e155
BenicioDelDenys <morkovkaden@gmail.com>
2020-05-18 00:10:39 +0300

    Updates

---
+++ precache-manifest.a464e66830ba587a04f9685f8d90f117.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5b0ef47deeb6df2b6eaed4120d1d98",
+    "url": "/coolinar-react/index.html"
+  },
+  {
+    "revision": "ff73ec74a6fdd8d537d0",
+    "url": "/coolinar-react/static/css/main.703ab23c.chunk.css"
+  },
+  {
+    "revision": "47ee79eaae1da6b5249c",
+    "url": "/coolinar-react/static/js/2.cf8edac2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coolinar-react/static/js/2.cf8edac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff73ec74a6fdd8d537d0",
+    "url": "/coolinar-react/static/js/main.e2145674.chunk.js"
+  },
+  {
+    "revision": "f62652a43ef745fe7981",
+    "url": "/coolinar-react/static/js/runtime-main.27275665.js"
+  },
+  {
+    "revision": "1bfeb2290f4dfc2bbbf9206318954bb3",
+    "url": "/coolinar-react/static/media/footer-logo.1bfeb229.jpg"
+  },
+  {
+    "revision": "90e491c05e9e8d6f2723d6ccba2d4c3e",
+    "url": "/coolinar-react/static/media/main-food-pic.90e491c0.jpg"
+  },
+  {
+    "revision": "3b29eacbe358234589c1225939f0b83a",
+    "url": "/coolinar-react/static/media/mainLogo.3b29eacb.jpg"
+  }
+]);
